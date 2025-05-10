25d5f3439425ec26fff189ee2f4a05aafb24dfd8
Rock Zhang <rock.zhang.cn@gmail.com>
2025-05-10 20:24:09 +1200

    Fix file

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { useEffect } from 'react';
 import AOS from 'aos';
 import "aos/dist/aos.css";
-import './index.css';
 import {
   BrowserRouter as Router,
   Routes,
@@ -12,7 +11,7 @@ import Home from './pages/Home';
 import Contact from './pages/Contact';
 import DemoProduct from './pages/DemoProduct';
 
-import {useDocTitle} from './components/CustomHook';
+import { useDocTitle } from './components/CustomHook';
 import ScrollToTop from './components/ScrollToTop';
 
 function App() {
@@ -30,7 +29,7 @@ function App() {
     });
   }, []);
 
-  useDocTitle("MLD | Molad e Konsult - Bespoke Web and Mobile Applications");
+  useDocTitle("Manyou Global");
 
   return (
     <>
@@ -39,7 +38,7 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/contact" element={<Contact />} />
-            <Route path="/get-demo" element={<DemoProduct />} /> 
+            <Route path="/get-demo" element={<DemoProduct />} />
           </Routes>
         </ScrollToTop>
       </Router>
